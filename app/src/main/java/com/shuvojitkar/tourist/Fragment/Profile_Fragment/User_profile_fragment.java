d42ec1b6ab2b7416afa7b5218b315aff33758912
shuvojit007 <shuvojitkar241@gmail.com>
2017-08-02 14:55:21 +0600

    commit #9 Add fab button on profile activity

--- app/src/main/java/com/shuvojitkar/tourist/Fragment/Profile_Fragment/User_profile_fragment.java
+++ app/src/main/java/com/shuvojitkar/tourist/Fragment/Profile_Fragment/User_profile_fragment.java
@@ -2,10 +2,15 @@ package com.shuvojitkar.tourist.Fragment.Profile_Fragment;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.shuvojitkar.tourist.R;
 
@@ -14,9 +19,37 @@ import com.shuvojitkar.tourist.R;
  */
 
 public class User_profile_fragment extends Fragment {
-    @Nullable
+    private  RecyclerView mUserProRec;
+    private FloatingActionButton mUserProfab;
+    View v;
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.profile_user_profile_fragment,container,false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, final Bundle savedInstanceState) {
+        v = inflater.inflate(R.layout.profile_user_profile_fragment,container,false);
+        init(v);
+
+      mUserProfab.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+                      .setAction("Action", null).show();
+
+
+                AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+                        final View dialogView = getActivity().getLayoutInflater().inflate(R.layout.user_post_dailog,null);
+                        builder.setView(dialogView);
+                        final AlertDialog dialog = builder.create();
+                        dialog.show();
+          }
+
+      });
+    
+
+        return v;
+    }
+
+    private void init(View v) {
+        mUserProfab = (FloatingActionButton) v.findViewById(R.id.profile_fab_btn);
+     /*   mUserProRec = (RecyclerView) v.findViewById(R.id.profile_post_rec);
+*/
     }
 }
