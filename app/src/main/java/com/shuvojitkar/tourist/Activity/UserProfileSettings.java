0ad102642b93ce92b6a83faff66a93c02374c17c
Shuvojit007 <shuvojitkar241@gmail.com>
2017-08-04 22:59:25 +0600

    commit #13 Hackathon commit

--- app/src/main/java/com/shuvojitkar/tourist/Activity/UserProfileSettings.java
+++ app/src/main/java/com/shuvojitkar/tourist/Activity/UserProfileSettings.java
@@ -242,7 +242,7 @@ public class UserProfileSettings extends AppCompatActivity {
                         }
                     }
                 });
-                //===============
+
 
 
             }
